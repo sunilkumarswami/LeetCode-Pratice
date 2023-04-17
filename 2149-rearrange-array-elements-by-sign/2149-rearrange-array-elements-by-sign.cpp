9106391ef79d70901b769097ffab0d6c0971d9cb
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-04-17 18:03:26 +0530

    Time: 230 ms (62.16%), Space: 123.9 MB (86.68%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,36 +3,18 @@ public:
     vector<int> rearrangeArray(vector<int>& a) {
         int n=a.size();
         vector<int> b(n,0);
-	    int cntPos=0,cntNeg=0;
-	    for(int k=0;k<n;k++){
-	        if(a[k]>=0) 
-	            cntPos++;
-	        else 
-	            cntNeg++;
-	    }
-	    int m=min(cntPos,cntNeg);
+	    
 	    int i=0,j=0;
 	    for(int k=0;k<n;k++){
 	        if(a[k]>=0){
-	            if(i<m){
-	                b[2*i]=a[k];
-	            }
-	            if(i==m) 
-	                i=2*i;
-	            if(i>=m)
-	            b[i]=a[k];
+	            b[2*i]=a[k];
 	            i++;
 	        }
 	        else{
-	            if(j<m){
-	                b[2*j+1]=a[k];
-	            }
-	            if(j==m) 
-	                j=2*j;
-	            if(j>=m)
-	            b[j]=a[k];
+	            
+	            b[2*j+1]=a[k];
 	            j++;
-	        }
+	        } 
 	    }
 	    return b;
     }
