cd8b9adf53718bb442f5152d203d5092c0f8b2ff
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-05-04 13:04:40 +0530

    Time: 189 ms (32.13%), Space: 46.3 MB (60.36%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -2,50 +2,22 @@ class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& a) {
         int n=a.size(),m=a[0].size();
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            int l=0,h=m-1;
+        int l=0,h=m-1;
         while(l<=h){
-            int mid=(l+h)/2;
-            if(mid-1>=0 && a[i][mid-1]>a[i][mid]){
-                h=mid-1;
+            int mid=l+(h-l)/2;
+            int maxR=0;
+            for(int i=1;i<n;i++){
+                if(a[maxR][mid]<a[i][mid])
+                    maxR=i;
             }
-            else if(mid+1<m && a[i][mid+1]>a[i][mid]){
-                l=mid+1;
-            }
-            else {
-                // cout<<mid<<endl;
-                if((i-1>=0 && a[i-1][mid]>a[i][mid]) || (i+1<n && a[i+1][mid]>a[i][mid])) l=mid+1;
-                else{
-                ans.push_back(i);
-                ans.push_back(mid);
-                return ans;
-                }
-            }
-        }
-             l=0,h=m-1;
-        while(l<=h){
-            int mid=(l+h)/2;
+            bool leftIsBig=mid-1>=0 && (a[maxR][mid-1]>a[maxR][mid]);
+            bool rightIsBig=mid+1<m && (a[maxR][mid+1]>a[maxR][mid]);
             
-            if(mid+1<m && a[i][mid+1]>a[i][mid]){
-                l=mid+1;
-            }
-            else if(mid-1>=0 && a[i][mid-1]>a[i][mid]){
-                h=mid-1;
-            }
-            else {
-                // cout<<mid<<endl;
-                if((i-1>=0 && a[i-1][mid]>a[i][mid]) || (i+1<n && a[i+1][mid]>a[i][mid])) h=mid-1;
-                else{
-                ans.push_back(i);
-                ans.push_back(mid);
-                return ans;
-                }
-            }
+            if(!leftIsBig && !rightIsBig)
+                return vector<int> {maxR,mid};
+            else if(leftIsBig) h=mid-1;
+            else l=mid+1;
         }
-        }
-        ans.push_back(-1);
-                ans.push_back(-1);
-        return ans;
+        return vector<int> {-1,-1};
     }
 };
