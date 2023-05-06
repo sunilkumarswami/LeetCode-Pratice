908881a6a6f18c63afb449ac39290ad47609b3b3
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-05-06 16:12:57 +0530

    Added solution - LeetHub

--- Largest odd number in string - GFG/largest-odd-number-in-string.cpp
+++ Largest odd number in string - GFG/largest-odd-number-in-string.cpp
@@ -9,7 +9,7 @@ class Solution {
         // your code here
         int j=-1;
         for(int i=s.size()-1;i>=0;i--){
-            if((s[i]-'0')%2!=0){
+            if(s[i]%2!=0){
                 j=i;
                 break;
             }
