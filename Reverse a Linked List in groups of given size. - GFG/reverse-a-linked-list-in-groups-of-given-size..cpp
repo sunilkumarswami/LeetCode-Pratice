7f9c437396ee65742afbe60573ce5726651c8e5d
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-05-17 17:43:22 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size. - GFG/reverse-a-linked-list-in-groups-of-given-size..cpp
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        	if(!head || !head->next) return head;
+        node* dummy=new node(0);
+        dummy->next=head;
+        node *pre=dummy,*cur=head,*nex=cur->next,*p=head;
+        int cnt=0;
+        while(p){
+            p=p->next;
+            cnt++;
+        }
+        while(cnt>0){
+            int x=k-1;
+            if(cnt<k) x=cnt-1;
+            while(x--){
+                cur->next=nex->next;
+                nex->next=pre->next;
+                pre->next=nex;
+                nex=cur->next;
+            }
+            cnt-=k;
+            if(cnt<=0) break;
+            pre=cur;
+            cur=nex;
+            nex=nex->next;
+            
+        }
+        return dummy->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+
+// } Driver Code Ends
