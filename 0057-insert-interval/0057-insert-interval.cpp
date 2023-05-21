e41172a2b5273a060e317e18d4df6d2b28430dbc
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-05-21 17:06:22 +0530

    Time: 19 ms (52.53%), Space: 17 MB (89.39%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& b) {
-        a.push_back(b);
-        sort(a.begin(),a.end());
+    vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& ni) {
         vector<vector<int>> ans;
-        ans.push_back(a[0]);
-        for(int i=1;i<a.size();i++){
-            if(ans.back()[1]>=a[i][0]){
-                ans.back()[1]=max(ans.back()[1],a[i][1]);
-            }
-            else ans.push_back(a[i]);
+        int n=a.size();
+        if(n==0) {
+            ans.push_back(ni);
+            return ans;
         }
-        return ans;
+        
+        bool flag=true;
+        for(int i=0;i<n;i++){
+            if(flag){
+                if(a[i][1]<ni[0]){
+                    ans.push_back(a[i]);
+                }
+                else{
+                    flag=false;
+                    if(ni[1]>=a[i][0])
+                    ans.push_back({min(ni[0],a[i][0]),max(ni[1],a[i][1])});
+                    else{
+                        ans.push_back(ni);
+                        ans.push_back(a[i]);
+                    }
+                }
+            }
+            else{
+                if(a[i][0]<=ans.back()[1]){
+                    ans.back()[1]=max(ans.back()[1],a[i][1]);
+                }
+                else ans.push_back(a[i]);
+            }
+        }
+        if(a[n-1][1]<ni[0]) ans.push_back(ni);
+        return ans; 
     }
 };
