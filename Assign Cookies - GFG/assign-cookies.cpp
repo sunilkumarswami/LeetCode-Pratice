bb2d3eacc889853ddc1bb984503bdb02ce082a77
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-05-22 19:20:39 +0530

    Added solution - LeetHub

---
+++ Assign Cookies - GFG/assign-cookies.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxChildren(int N, int M, vector<int> &greed, vector<int> &sz) {
+        // code here
+        sort(greed.begin(),greed.end());
+        sort(sz.begin(),sz.end());
+        int j=M-1,i=N-1,ans=0;
+        while(i>=0 && j>=0){
+            if(greed[i]<=sz[j]){
+                ans++;
+                i--,j--;
+            }
+            else
+            i--;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<int> greed(N), sz(M);
+        for (int i = 0; i < N; i++) {
+            cin >> greed[i];
+        }
+        for (int i = 0; i < M; i++) {
+            cin >> sz[i];
+        }
+        Solution obj;
+        cout << obj.maxChildren(N, M, greed, sz) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
