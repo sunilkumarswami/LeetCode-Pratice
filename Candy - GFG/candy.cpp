eeb55e675ccd3807b3f093ee5ccf56bc6b657ba5
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-05-22 16:15:22 +0530

    Added solution - LeetHub

---
+++ Candy - GFG/candy.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minCandy(int n, vector<int> &rat) {
+        // int n=rat.size();
+        vector<int> v(n,1);
+        for(int i=1;i<n;i++){
+            if(rat[i]>rat[i-1]){
+                v[i]=v[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(rat[i]>rat[i+1] && v[i]<=v[i+1]){
+                v[i]=v[i+1]+1;
+            }
+        }
+        int ans=0;
+        for(auto x:v) ans+=x;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> ratings(N);
+        for (int i = 0; i < N; i++) {
+            cin >> ratings[i];
+        }
+        Solution obj;
+        cout << obj.minCandy(N, ratings) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
