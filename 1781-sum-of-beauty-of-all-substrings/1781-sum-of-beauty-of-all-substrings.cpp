745da07daccc0658d546e2193553b9f1a5fa2bc6
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-05-26 18:32:40 +0530

    Time: 1065 ms (5.03%), Space: 176.2 MB (5.04%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -3,17 +3,14 @@ public:
     int beautySum(string s) {
         int n=s.size(),ans=0;
         for(int i=0;i<n;i++){
-            unordered_map<int,int> mp;
+            map<char,int> mp;
+            multiset<int> st;
             for(int j=i;j<n;j++){
+                if(mp.find(s[j])!=mp.end())
+                st.erase(st.find(mp[s[j]]));
                 mp[s[j]]++;
-                int maxi=1,mini=n;
-                for(auto it:mp){
-                    if(maxi<it.second)
-                        maxi=it.second;
-                    if(mini>it.second)
-                        mini=it.second;
-                }
-                ans+=maxi-mini;
+                st.insert(mp[s[j]]);
+                ans+=*st.rbegin()-*st.begin();
             }
         }
         return ans;
