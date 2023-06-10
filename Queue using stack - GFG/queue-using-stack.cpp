f0c120ccc4bbf04c677987429f101785d57c50b1
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-06-10 14:09:55 +0530

    Added solution - LeetHub

---
+++ Queue using stack - GFG/queue-using-stack.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Queue {
+    stack<int> input, output;
+public:
+    stack<int> st1,st2;
+    void enqueue(int x) {
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        st2.push(x);
+        while(!st2.empty()){
+            st1.push(st2.top());
+            st2.pop();
+        }
+    }
+
+    int dequeue() {
+        if(st1.empty()) return -1;
+        int ele=st1.top();
+        st1.pop();
+        return ele;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Queue ob;
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            ob.enqueue(a);
+        }
+        else if(QueryType==2)
+        {
+            cout<<ob.dequeue()<<" ";
+
+        }
+        }
+    cout<<endl;
+    }
+}
+
+// } Driver Code Ends
