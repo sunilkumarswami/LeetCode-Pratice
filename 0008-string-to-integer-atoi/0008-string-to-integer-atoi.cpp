4975dbaef40558f1b73952a11ee92a62904d9a0f
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-06-13 19:16:29 +0530

    Time: 11 ms (6.77%), Space: 9.6 MB (5.01%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,8 +1,43 @@
 class Solution {
 public:
+    void solve(int i,string s,int n,bool &neg,bool &flag,long long &ans){
+        if(i>=n || (!flag && !('0'<=s[i] && s[i]<='9'))) return;
+        
+         if(!(s[i]==' ')) flag=false;
+        if(s[i]==' ') { 
+            solve(i+1,s,n,neg,flag,ans);
+                      }
+        else if(s[i]=='-'){
+            neg=true;
+            flag=false;
+            solve(i+1,s,n,neg,flag,ans);
+        }
+        else if(s[i]=='+') solve(i+1,s,n,neg,flag,ans);
+        else if('0'<=s[i] && s[i]<='9') {
+           
+            ans=ans*10+(s[i]-'0');
+            if(neg && -ans<INT_MIN)
+            { 
+                ans= INT_MIN;
+                return;
+            }
+            if(!neg && ans>INT_MAX) {
+                ans= INT_MAX;
+            return;
+            }
+            solve(i+1,s,n,neg,flag,ans);
+        }
+           else
+           return;
+    }
+    
     int myAtoi(string s) {
         long long ans=0,temp=0;
-        int i=0,n=s.size(),neg=0;
+        int i=0,n=s.size();
+        bool flag=true,neg=false;
+        solve(i,s,n,neg,flag,ans);
+        if(neg && ans!=INT_MIN) return -ans;
+        return ans;
         while(i<n && s[i]==' ') i++;
         if(i<n && s[i]=='-'){
             neg=1;
