b7ef887e3fac304e5a3be6970c798ce243264e67
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-06-19 16:59:52 +0530

    Time: 422 ms (41.24%), Space: 14.3 MB (51.86%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int ladderLength(string start, string tar, vector<string>& wl) {
-        map<string,int> mp;
+        // map<string,int> mp;
         set<string> st;
-        mp[start]=1;
+        st.insert(start);
         for(int i=0;i<wl.size();i++){
-            mp[wl[i]]=0;
             st.insert(wl[i]);
         }
         queue<pair<string,int>> q;
         q.push({start,1});
-        
+        st.erase(start);
         while(!q.empty()){
             string s=q.front().first;
             int level=q.front().second;
@@ -22,8 +21,8 @@ public:
                 for(char ch='a';ch<='z';ch++){
                     if(ch!=s[i]){
                         s2[i]=ch;
-                        if(st.find(s2)!=st.end() && mp[s2]==0){
-                            mp[s2]=1;
+                        if(st.find(s2)!=st.end()){
+                            st.erase(s2);
                             q.push({s2,level+1});
                         }
                     }
