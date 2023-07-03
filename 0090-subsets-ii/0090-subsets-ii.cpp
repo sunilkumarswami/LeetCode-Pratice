411854e47d3d1e402bdc395c495005eca5408cc2
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-07-03 15:39:15 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (99.81%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     void solve(int ind,vector<int> &v,vector<int> &a,vector<vector<int>> &ans){
         ans.push_back(v);
-        if(ind==a.size()) {
-            return;}
         
         for(int i=ind;i<a.size();i++){
             if(i>ind && a[i]==a[i-1]) continue;
