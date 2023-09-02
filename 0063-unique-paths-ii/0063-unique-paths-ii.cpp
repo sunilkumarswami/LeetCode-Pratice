9da71d7cbdf6eaa77f80b546d3854c7c99702f29
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-09-02 19:13:46 +0530

    Time: 5 ms (37.84%), Space: 7.8 MB (67.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -16,6 +16,19 @@ public:
         int n=grid.size(),m=grid[0].size();
         if(grid[0][0] ==1 || grid[n-1][m-1]==1) return 0;
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,grid,dp);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else if(grid[i][j]==1) dp[i][j]= 0;
+                else{
+                    int up=0,left=0;
+                    if(i-1>=0) up=dp[i-1][j];
+                    if(j-1>=0) left=dp[i][j-1];
+                    dp[i][j]= up+left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
