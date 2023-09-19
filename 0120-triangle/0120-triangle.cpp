ded5b231eb0c0ecc749a2ecb59b01c322dca915e
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2023-09-19 12:02:30 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (5.10%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>& tri,vector<vector<int>> &dp){
+        int n=tri.size(),m=tri[i].size();
+        if(i==n-1) return tri[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j] = tri[i][j]+min(solve(i+1,j,tri,dp),solve(i+1,j+1,tri,dp));
+    }
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n=tri.size();
+        vector<vector<int>> dp;
+        for(int i=0;i<n;i++){
+            vector<int> temp(tri[i].size(),-1);
+            dp.push_back(temp);
+        }
+        return solve(0,0,tri,dp);
+    }
+};
