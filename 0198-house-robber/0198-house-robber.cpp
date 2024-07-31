f3bbfa0c50edfa0b77bc78ee4146c0f13c800afe
Sunil Kumar <58823470+sunilkumarswami@users.noreply.github.com>
2024-07-31 17:02:00 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (6.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,13 +11,15 @@ public:
     int rob(vector<int>& a) {
         int n=a.size(),ans=0;
         vector<int> dp(n,-1);
-        dp[0]=a[0];
-        if(n>1)
-            dp[1]=max(dp[0],a[1]);
+        int sec=a[0],fir;
+        if(n==1) return sec;
+            fir=max(sec,a[1]);
         
         for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-1],a[i]+dp[i-2]);
+            int cur=max(fir,a[i]+sec);
+            sec=fir;
+            fir=cur;
         }
-        return dp[n-1];
+        return fir;
     }
 };
